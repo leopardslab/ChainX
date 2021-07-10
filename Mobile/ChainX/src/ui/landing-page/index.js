552370fab6d661fdeffc258be16db670832fe8d5
Esandhavithanage <esandhavithanage@gmail.com>
2021-07-10 17:06:17 +0530

    [Mobile] chnage the landing page behaviour

--- Mobile/ChainX/src/ui/landing-page/index.js
+++ Mobile/ChainX/src/ui/landing-page/index.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import { Text, View, Button, ImageBackground, StatusBar, ActivityIndicator } from 'react-native';
+import React, { useState, useRef, useEffect } from "react";
+// import { Text, View, Button, ImageBackground, StatusBar, ActivityIndicator } from 'react-native';
 import { LANDING_PAGE_BACKGROUND_IMAGE } from '../../images-ref'
 import { splashScreenStyles } from './styles'
+import { Progress, Box, Text, Divider, Flex, Center } from 'native-base';
 import { renderMessage } from '../../language/lang-switch'
 
+
+
 export class LandingPage extends React.Component {
-  state = {
-    loadingText: renderMessage("loadingPageMessage")
- }
+   
+
+  constructor(props){
+    super(props);
+    this.state = {
+      loadingText: renderMessage("loadingPageMessage"),
+      loadingProgressValue: 1
+    }
+  }
+  
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      100
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState(prevState => {
+      return {loadingProgressValue: prevState.loadingProgressValue + 10}
+    });
+    if(this.state.loadingProgressValue >= 100){
+      clearInterval(this.timerID);
+    }
+  }
+
+
   render() {
+   
     return (
-      <View style={splashScreenStyles.container}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="#4F6D7A"
-
-        ></StatusBar>
-
-        <ImageBackground
-          style={splashScreenStyles.image}
-          source={LANDING_PAGE_BACKGROUND_IMAGE}
-        >
-          <ActivityIndicator size="large" color="#00ff00" style={splashScreenStyles.loadingSpinner}>
-          </ActivityIndicator>
-          <Text style={splashScreenStyles.loadingText}>{this.state.loadingText}</Text>
-        </ImageBackground>
-
-      </View>
+      <Box w="100%">
+        <Center>
+          <Flex direction="row" p={3}>
+            <Text>{renderMessage("LoadingMessage01")}</Text>
+            <Divider bg="green.500" size={3} mx={4} orientation="vertical" />
+            <Text>{renderMessage("LoadingMessage02")}</Text>
+            <Divider bg="blue.500" size={3} mx={4} orientation="vertical" />
+            <Text>{renderMessage("LoadingMessage03")}</Text>
+          </Flex>
+        </Center>
+        <Progress colorScheme="emerald" size="lg" value={this.state.loadingProgressValue} mx={10} />
+      </Box>
     );
   }
 }
